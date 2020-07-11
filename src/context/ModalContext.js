8638d7fb368e6cf21ea823747644042c1d099b07
Miguel Somohano <miguel.spinis@gmail.com>
2020-07-10 22:50:42 -0500

    Agregando colsulta a la API para obtener receta

--- src/context/ModalContext.js
+++ src/context/ModalContext.js
@@ -9,6 +9,22 @@ const ModalProvider = (props) => {
 
     // State del provider, guardar id cuando el usuario de click
     const [idreceta, guardarIdReceta] = useState(null);
+    const [ receta, guardarReceta ] = useState({});
+
+    // Una vez que tenemos una receta llamar la API
+    useEffect(() => {
+        const obtenerReceta = async () => {
+            if(!idreceta) return;
+
+            const url = `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${idreceta}`;
+
+            const resultado = await axios.get(url);
+
+            guardarReceta(resultado.data.drinks[0]);
+        }
+        obtenerReceta();
+        
+    }, [idreceta]);
 
     return ( 
         <ModalContext.Provider
